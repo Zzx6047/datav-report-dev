3c97b7d472b3669838b29f4d7cc888567917b0a8
Zzx6047 <474969612@qq.com>
2020-06-18 23:31:38 +0800

    销售趋势图组件开发

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,6 +1,53 @@
 <!--  -->
 <template>
-  <ve-line :data="data"></ve-line>
+  <div class="sales-view">
+    <el-card shadow="hover">
+      <template v-slot:header>
+        <div class="menu-wrapper">
+          <el-menu
+            class="sales-view-menu"
+            :default-active="activeIndex"
+            mode="horizontal"
+            @select="onMenuSelect">
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group v-model="radioSelect" size="small">
+              <el-radio-button label="今日"></el-radio-button>
+              <el-radio-button label="本周"></el-radio-button>
+              <el-radio-button label="本月"></el-radio-button>
+              <el-radio-button label="今年"></el-radio-button>
+            </el-radio-group>
+            <el-date-picker
+              v-model="date"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              size="small"
+              unlink-panels
+              :picker-options="pickerOptions"
+              class="sales-view-date-picker">
+            </el-date-picker>
+          </div>
+        </div>
+      </template>
+      <template>
+        <div class="sales-view-chart-wrapper">
+          <v-chart :options="chartOption"></v-chart>
+          <div class="sales-view-list">
+            <div class="sales-view-title">排行榜</div>
+            <div class="list-item" v-for="item in rankData" :key="item.no">
+              <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">{{item.no}}</div>
+              <div class="list-item-name">{{item.name}}</div>
+              <div class="list-item-money">{{item.money}}</div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,20 +55,135 @@ export default {
   name: '',
   data () {
     return {
-      data: {
-        columns: ['日期', '销售额'],
-        rows: [
-          { 日期: '1月1日', 销售额: 123 },
-          { 日期: '1月2日', 销售额: 1233 },
-          { 日期: '1月3日', 销售额: 2123 },
-          { 日期: '1月4日', 销售额: 4123 },
-          { 日期: '1月5日', 销售额: 3123 },
-          { 日期: '1月6日', 销售额: 7123 }
-        ]
-      }
+      activeIndex: '1',
+      radioSelect: '今日',
+      date: null,
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近一个月',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近三个月',
+          onClick (picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+            picker.$emit('pick', [start, end])
+          }
+        }]
+      },
+      chartOption: {},
+      rankData: [
+        {
+          no: 1,
+          name: '肯德基',
+          money: '323,234'
+        },
+        {
+          no: 2,
+          name: '肯德基',
+          money: '323,234'
+        },
+        {
+          no: 3,
+          name: '肯德基',
+          money: '323,234'
+        },
+        {
+          no: 4,
+          name: '肯德基',
+          money: '323,234'
+        },
+        {
+          no: 5,
+          name: '肯德基',
+          money: '323,234'
+        },
+        {
+          no: 6,
+          name: '肯德基',
+          money: '323,234'
+        },
+        {
+          no: 7,
+          name: '肯德基',
+          money: '323,234'
+        }
+      ]
+    }
+  },
+  methods: {
+    onMenuSelect (index) {
+      this.activeIndex = index
     }
   }
 }
 </script>
 <style lang='scss' scoped>
+.sales-view {
+  margin-top: 20px;
+  .menu-wrapper {
+    position: relative;
+    display: flex;
+    .sales-view-menu {
+      width: 100%;
+      padding-left: 20px;
+      .el-menu-item {
+        height: 50px;
+        line-height: 50px;
+        margin: 0 20px;
+      }
+    }
+    .menu-right {
+      position: absolute;
+      top: 0;
+      right: 20px;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      .sales-view-date-picker {
+        margin-left: 20px;
+      }
+    }
+  }
+  .sales-view-chart-wrapper {
+    display: flex;
+    height: 270px;
+    .echarts {
+      flex: 0 0 70%;
+      width: 70%;
+      height: 100%;
+    }
+    .sales-view-list {
+      flex: 1;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      margin-top: 15px;
+      .sales-view-title {
+        margin-top: 20px;
+        font-size: 12px;
+        color: #666;
+        font-weight: 500;
+      }
+      .list-item {
+        display: flex;
+      }
+    }
+  }
+}
 </style>
