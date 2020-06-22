445c3677efa7375436ce9560df4aa27c2985115e
zhaojinxin <474969612@qq.com>
2020-06-22 16:58:56 +0800

    bottom-view组件完成

--- src/components/BottomView/index.vue
+++ src/components/BottomView/index.vue
@@ -8,26 +8,29 @@
         </template>
         <template>
           <div class="chart-wrapper">
-            <div class="chart">
-              <div class="chart-title">搜索用户数</div>
-              <div class="chart-title">93,634</div>
-              <v-chart :options="searchUserOption"></v-chart>
+            <div class="chart-inner">
+              <div class="chart">
+                <div class="chart-title">搜索用户数</div>
+                <div class="chart-data">93,634</div>
+                <v-chart :options="searchUserOption"></v-chart>
+              </div>
+              <div class="chart">
+                <div class="chart-title">搜索量</div>
+                <div class="chart-data">198,782</div>
+                <v-chart :options="searchNumberOption"></v-chart>
+              </div>
             </div>
-            <div class="chart">
-              <div class="chart-title">搜索量</div>
-              <div class="chart-title">198,782</div>
-              <v-chart :options="searchNumberOption"></v-chart>
+            <div class="tabel-wrapper">
+              <el-table :data="tableData">
+                <el-table-column prop="rank" label="排名" width="180"></el-table-column>
+                <el-table-column prop="keyword" label="关键词" width="180"></el-table-column>
+                <el-table-column prop="count" label="总搜索量"></el-table-column>
+                <el-table-column prop="users" label="总搜索用户"></el-table-column>
+              </el-table>
+              <el-pagination
+                layout="prev, pager, next" :total="100" :page-size="4" background @current-change="onPageChange">
+              </el-pagination>
             </div>
-          </div>
-          <div class="tabel-wrapper">
-            <el-table :data="tableData">
-              <el-table-column prop="prop" label="label" width="width">
-              </el-table-column>
-            </el-table>
-            <el-pagination
-              layout="prev, pager, next"
-              :total="50">
-            </el-pagination>
           </div>
         </template>
       </el-card>
@@ -46,7 +49,9 @@
           </div>
         </template>
         <template>
-          <v-chart :options="categoryOptions"></v-chart>
+          <div class="chart-wrapper">
+            <v-chart :options="categoryOptions"></v-chart>
+          </div>
         </template>
       </el-card>
     </div>
@@ -58,11 +63,195 @@ export default {
   name: '',
   data () {
     return {
-      searchUserOption: {},
-      searchNumberOption: {},
-      tableData: [],
+      searchUserOption: {
+        xAxis: {
+          type: 'category',
+          boundaryGap: false
+        },
+        yAxis: {
+          show: false,
+          min: 0,
+          max: 300
+        },
+        series: [{
+          type: 'line',
+          data: [100, 150, 200, 250, 200, 150, 100, 50, 100, 150],
+          areaStyle: {
+            color: 'rgba(95,187,255,.5)'
+          },
+          lineStyle: {
+            color: 'rgba(95,187,255)'
+          },
+          itemStyle: {
+            opacity: 0
+          },
+          smooth: true
+        }],
+        grid: {
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0
+        }
+      },
+      searchNumberOption: {
+        xAxis: {
+          type: 'category',
+          boundaryGap: false
+        },
+        yAxis: {
+          show: false,
+          min: 0,
+          max: 300
+        },
+        series: [{
+          type: 'line',
+          data: [100, 150, 200, 250, 200, 150, 100, 50, 100, 150],
+          areaStyle: {
+            color: 'rgba(95,187,255,.5)'
+          },
+          lineStyle: {
+            color: 'rgba(95,187,255)'
+          },
+          itemStyle: {
+            opacity: 0
+          },
+          smooth: true
+        }],
+        grid: {
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0
+        }
+      },
+      tableData: [
+        {
+          id: 1, rank: 1, keyword: '北京', count: 100, users: 90, range: '90%'
+        },
+        {
+          id: 2, rank: 2, keyword: '北京', count: 100, users: 90, range: '90%'
+        },
+        {
+          id: 3, rank: 3, keyword: '北京', count: 100, users: 90, range: '90%'
+        },
+        {
+          id: 4, rank: 4, keyword: '北京', count: 100, users: 90, range: '90%'
+        }
+      ],
       radioSelect: '品类',
       categoryOptions: {}
+    }
+  },
+  mounted () {
+    this.renderPieChart()
+  },
+  methods: {
+    onPageChange (page) {
+      console.log(page)
+    },
+    renderPieChart () {
+      const mockData = [
+        {
+          legendname: '粉面粥店',
+          value: 67,
+          percent: '15.40',
+          itemStyle: {
+            color: '#e7e702'
+          },
+          name: '粉面粥店 | 15.40%'
+        },
+        {
+          legendname: '简餐便当',
+          value: 97,
+          percent: '22.30',
+          itemStyle: {
+            color: '#8d7fec'
+          },
+          name: '简餐便当 | 22.30%'
+        },
+        {
+          legendname: '汉堡披萨',
+          value: 92,
+          percent: '21.15',
+          itemStyle: {
+            color: '#5085f2'
+          },
+          name: '汉堡披萨 | 21.15%'
+        }
+      ]
+      this.categoryOptions = {
+        title: [{
+          text: '品类分布',
+          textStyle: {
+            fontSize: 14,
+            color: '#666'
+          },
+          left: 20,
+          top: 20
+        }, {
+          text: '累计订单量',
+          subtext: '320',
+          x: '34.5%',
+          y: '42.5%',
+          textStyle: {
+            fontSize: 14,
+            color: '#999'
+          },
+          subtextStyle: {
+            fontSize: 28,
+            color: '#333'
+          },
+          textAlign: 'center'
+        }],
+        series: [{
+          name: '品类分布',
+          type: 'pie',
+          data: mockData,
+          label: {
+            normal: {
+              show: true,
+              position: 'outter',
+              formatter: function (params) {
+                return params.data.legendname
+              }
+            }
+          },
+          center: ['35%', '50%'],
+          radius: ['45%', '60%'],
+          labelLine: {
+            normal: {
+              length: 5,
+              length2: 3,
+              smooth: true
+            }
+          },
+          clockwise: false,
+          itemStyle: {
+            borderWidth: 4,
+            borderColor: '#fff'
+          }
+        }],
+        legend: {
+          type: 'scroll',
+          orient: 'vertical',
+          height: 250,
+          left: '70%',
+          top: 'middle',
+          textStyle: {
+            color: '#888'
+          }
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: function (params) {
+            return params.seriesName + '<br/>' +
+            params.marker + params.data.legendname + '<br/>' +
+            '数量：' + params.data.value + '<br/>' +
+            '占比：' + params.data.percent + '%'
+          }
+        }
+      }
     }
   }
 }
@@ -73,6 +262,14 @@ export default {
   margin-top: 20px;
   .view {
     flex: 1;
+    box-sizing: border-box;
+    width: 50%;
+    &:first-child {
+      padding: 0 10px 0 0;
+    }
+    &:last-child {
+      padding: 0 0 0 10px;
+    }
     .title-wrapper {
       display: flex;
       align-items: center;
@@ -82,6 +279,49 @@ export default {
       font-size: 14px;
       font-weight: 500;
       padding: 0 0 0 20px;
+      .radio-wrapper {
+        display: flex;
+        flex: 1;
+        justify-content: flex-end;
+        padding-right: 20px;
+      }
+    }
+    .chart-wrapper {
+      display: flex;
+      flex-direction: column;
+      height: 452px;
+      .chart-inner {
+        display: flex;
+        padding: 0 10px;
+        margin-top: 20px;
+        .chart {
+          flex: 1;
+          padding: 0 10px;
+          .chart-title {
+            color: #999;
+            font-size: 14px;
+          }
+          .chart-data {
+            font-size: 22px;
+            color: #333;
+            letter-spacing: 2px;
+            font-weight: 500;
+          }
+          .echarts {
+            height: 50px;
+          }
+        }
+      }
+      .tabel-wrapper {
+        flex: 1;
+        margin-top: 20px;
+        padding: 0 20px 20px;
+        .el-pagination {
+          display: flex;
+          justify-content: flex-end;
+          margin-top: 15px;
+        }
+      }
     }
   }
 }
